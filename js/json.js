a7cde1ca6b21011c14a344e2a3befb0837ee1e2a
Patrik Näsström <patrik_nasstrom@hotmail.com>
2019-12-14 14:57:27 +0100

    added updateChart function and getTotalUser

--- js/json.js
+++ js/json.js
@@ -1,56 +1,93 @@
-$(function() {
+//------------------------------- updateChart ---------------------------------------------------
 
-
-    //  console.log('hej')
+async function updateChart(salesChart) {
+    try {
+        // salesChart=salesChart
+        const response = await fetch(`https://inlupp-fa.azurewebsites.net/api/total-users`)
+        const data = await response.json()
+        // console.log(data)
     
+        let datasetAPI = data.dataset
+        // let key=array
+        // console.log('***Dataset*****')
+        // console.log(datasetAPI)
 
-    var fullName= getFullName('Patrik', 'Näsström')
-    getTotalAmount(`https://inlupp-fa.azurewebsites.net/api/total-sales`, "totalSales")
-    getTotalAmount(`https://inlupp-fa.azurewebsites.net/api/total-purchases`, "totalPurchases")
-    getTotalAmount(`https://inlupp-fa.azurewebsites.net/api/total-orders`, "totalOrders")
-    getTotalAmount(`https://inlupp-fa.azurewebsites.net/api/total-growth`, "totalGrowth")
-    // getTotalPurchases()
-//------------------------------- getFullName ---------------------------------------------------
-    function getFullName(firstName, lastName){
-        fetch(`https://inlupp-fa.azurewebsites.net/api/users?firstname=${firstName}&lastname=${lastName}`)
-        .then(response=>response.text())
-        .then(data=>{
-            console.log(data)
+        salesChart.data.datasets[0].data=datasetAPI.data
+        salesChart.data.labels=datasetAPI.labels
+        salesChart.update();
+        
+    } catch (error) { console.log(error) }
+
+   
+}
+ //------------------------------- getFullName ---------------------------------------------------
+ function getFullName(firstName, lastName) {
+    fetch(`https://inlupp-fa.azurewebsites.net/api/users?firstname=${firstName}&lastname=${lastName}`)
+        .then(response => response.text())
+        .then(data => {
+            // console.log(data)
             document.getElementById("fullName").innerHTML = "Hej " + data + " ,Welcome back!";
+            // console.log("getFullName") //used for debugging
             return data
         })
-    }
+}
 //------------------------------- getTotalAmount ---------------------------------------------------
-    function getTotalAmount(url, id){
-        // fetch(`https://inlupp-fa.azurewebsites.net/api/total-sales`)
-        fetch(url)
-        .then(response=>response.json())
-        .then(data=>{
+function getTotalAmount(url, id) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
             // console.log(data)
-            let key1=data.currency
-            let key2=data.amount
+            let key1 = data.currency
+            let key2 = data.amount
             // console.log(key1)
             // console.log(key2)
 
             document.getElementById(id).innerHTML = key1 + key2;
+            // console.log("getAmount" + id)   //used for debugging
             return data
         })
-    }
+}
 //------------------------------- getTotalProjects ---------------------------------------------------
 
-    // function getTotalPurchases(){
-    //     fetch(`hhttps://inlupp-fa.azurewebsites.net/api/total-purchases`)
-    //     .then(response=>response.json())
-    //     .then(data=>{
-    //         console.log(data)
-    //         let key1=data.currency
-    //         let key2=data.amount
+function getTotalUsers() {
+    fetch(`https://inlupp-fa.azurewebsites.net/api/total-users`)
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data)
+            let key1 = "" + data.users
+            let key2 = data.growth
+            // let key3 = data.dataset
+            // console.log(key1)
+            let sliceOne = key1.slice(2, 5)
+            let sliceTwo = key1.slice(0, 2)
+            // console.log(sliceOne)
+            // console.log(sliceTwo)
+            // console.log(key3)
 
-    //         console.log(key1)
-    //         console.log(key2)
-    
-    //         document.getElementById("totalProjects").innerHTML = key1 + key2;
-    //         return data
-    //     })
-    // }
+            // var labels=key3.labels
+            // console.log(labels)
+
+            // var labels=key3.labels
+            // console.log(labels)
+
+            document.getElementById("users").innerHTML = sliceTwo + "," + sliceOne;
+            document.getElementById("growth").innerHTML = "+" + key2 + "%";
+
+            // console.log("getTotalUsers") //used for debugging
+
+            return data
+        })
+}
+
+$(function () {
+
+    getFullName('Patrik', 'Näsström')
+    getTotalAmount(`https://inlupp-fa.azurewebsites.net/api/total-sales`, "totalSales")
+    getTotalAmount(`https://inlupp-fa.azurewebsites.net/api/total-purchases`, "totalPurchases")
+    getTotalAmount(`https://inlupp-fa.azurewebsites.net/api/total-orders`, "totalOrders")
+    getTotalAmount(`https://inlupp-fa.azurewebsites.net/api/total-growth`, "totalGrowth")
+    getTotalUsers()   
+   
+   
+   
 })
